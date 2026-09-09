940ce42a81e432d09318ca34d937844da2e43dea
sankalp6115 <sankalpomar6115@gmail.com>
2026-09-09 20:54:11 +0530

    Setup analytics

---
+++ analytics.js
@@ -0,0 +1,12 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+// Initialize immediately
+gtag('js', new Date());
+gtag('config', 'G-QGN70LHYM8');
+
+// Load GA script
+const script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-QGN70LHYM8";
+document.head.appendChild(script);
